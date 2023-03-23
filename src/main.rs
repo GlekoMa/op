7ae843acbd37a3f64b162aaf4b984094c7be0aa8
GlekoMa <niko12374@icloud.com>
2023-03-23 12:35:49 +0800

    fix "op code/" and handle more error situation

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,15 @@ use std::path::Path;
 use std::process::{exit, Command};
 
 fn main() {
+    // 0. get env args
     let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        ep("need one path argument");
+        exit(1);
+    }
 
-    // convert args[1] to Path
-    let path_str = {
-        let t = &args[1].replace('~', "/home/gleko");
-        if t == "." {
-            getcwd()
-        } else if !t.contains('/') {
-            format!("./{t}")
-        } else {
-            t.to_owned()
-        }
-    };
+    // 1. convert args[1] to Path
+    let path_str = deal_kinds_of_path(args[1].clone());
     let path = Path::new(&path_str);
     println!("Oops {:?}", path); // This code is for debug.
     if !path.exists() {
@@ -24,7 +20,7 @@ fn main() {
         exit(1);
     };
 
-    // parse path to parent and file_name
+    // 2. parse path to parent and file_name
     let parent = path.parent().unwrap_or_else(|| {
         ep("parse `path.parent`");
         exit(1);
@@ -41,13 +37,13 @@ fn main() {
             exit(1);
         });
 
-    // cd parent path
+    // 3. cd parent path
     env::set_current_dir(parent).unwrap_or_else(|_err| {
         ep("set dir");
         exit(1);
     });
 
-    // execute open (using exeplorer.exe) cmd
+    // 4. execute open (using exeplorer.exe) cmd
     let cmd_open = format!("explorer.exe {file_name}");
     Command::new("sh")
         .arg("-c")
@@ -58,7 +54,7 @@ fn main() {
             exit(1)
         });
 
-    // if the file make people happy, print a happy wish.
+    // *. if the file make people happy, print a happy wish.
     if path.extension().is_none() {
     } else {
         let ext = path
@@ -81,6 +77,35 @@ fn main() {
 
 fn ep(msg: &str) {
     eprintln!("{}: {}", "error".red(), msg);
+}
+
+fn deal_kinds_of_path(mut p: String) -> String {
+    // when last char is '/', delete it
+    // if not, type "op code/" will raise an error
+    // because there is no parent path
+    let p_last_char = p.chars().last().unwrap_or_else(|| {
+        ep("path is empty");
+        exit(1);
+    });
+    if p_last_char == '/' {
+        p.pop();
+    }
+
+    // detail
+    if p.contains('~') {
+        // type 'op ~' = type 'op /home/gleko/'
+        // because my user name is gleko
+        p.replace('~', "/home/gleko").to_owned()
+    } else if p == "." {
+        // type 'op .', open current dir
+        getcwd()
+    } else if !p.contains('/') {
+        // type 'op code' = type 'op ./code'
+        format!("./{p}")
+    } else {
+        // general situation
+        p.to_owned()
+    }
 }
 
 fn getcwd() -> String {
